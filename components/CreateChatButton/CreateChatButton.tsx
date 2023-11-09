19f592e140de2176b0122fd6770b92ee9756daa8
ofir <ofir@som.co.il>
2023-11-09 13:33:54 +0200

    clean code

--- components/CreateChatButton/CreateChatButton.tsx
+++ components/CreateChatButton/CreateChatButton.tsx
@@ -1,14 +1,14 @@
-"use client"
+'use client';
 
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { MessageSquarePlusIcon, MessagesSquareIcon } from 'lucide-react';
+import { MessageSquarePlusIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
 const CreateChatButton = () => {
   const router = useRouter();
 
-  const createNewChat = async() => {
+  const createNewChat = async () => {
     router.push('/chat/abc');
   };
 
--- components/UserAvatar/UserAvatar.tsx
+++ components/UserAvatar/UserAvatar.tsx
@@ -1,35 +1,37 @@
 import React from 'react';
-import {cn} from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
-import {className} from "postcss-selector-parser";
-import Image from "next/image";
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { className } from 'postcss-selector-parser';
+import Image from 'next/image';
 
 function UserAvatar(props: PropsState) {
   return (
-    <Avatar className={cn("bg-white text-black", props.className)}>
-      {
-        props.image &&
-          <Image
-              src={props.image || ""}
-              alt={props.name || ""}
-              width={40}
-              height={40}
-              className="rounded-full"/>
-      }
+    <Avatar className={cn('bg-white text-black', props.className)}>
+      {props.image && (
+        <Image
+          src={props.image || ''}
+          alt={props.name || ''}
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+      )}
       {/*<AvatarImage src="https://github.com/shadcn.png"/>*/}
-      <AvatarFallback className="dark:bg-white dark:text-black text-lg">{
-        props?.name?.split(" ").map((n) => n[0]).join("")
-      }</AvatarFallback>
+      <AvatarFallback className="dark:bg-white dark:text-black text-lg">
+        {props?.name
+          ?.split(' ')
+          .map((n) => n[0])
+          .join('')}
+      </AvatarFallback>
     </Avatar>
   );
 }
 
 interface PropsState {
-  name: string,
-  image: string,
-  className?: string
-
+  name: string;
+  image: string;
+  className?: string;
 }
 
 export default UserAvatar;
