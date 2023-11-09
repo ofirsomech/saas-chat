c81ab1ef44b8d269f5a6009fc74e2343b6b0dbfe
ofir <ofir@som.co.il>
2023-11-09 13:31:40 +0200

    Create chat btn and start crate the main page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,6 +6,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/auth';
 import Link from 'next/link';
 import { MessagesSquareIcon } from 'lucide-react';
+import CreateChatButton from "@/components/CreateChatButton/CreateChatButton";
 
 export default async function Header() {
   const session = await getServerSession(authOptions);
@@ -15,23 +16,24 @@ export default async function Header() {
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
-        <div className="flex-1 items-center justify-end space-x-4">
+        <div className="flex-1 flex items-center justify-end space-x-4">
           {/* LanguageSelect */}
           {session ? (
             <>
               <Link href={'/chat'} prefetch={false}>
                 <MessagesSquareIcon className="text-black dark:text-white" />
               </Link>
+              <CreateChatButton />
             </>
           ) : (
-            <>
-              <Link href="/pricing">Pricing</Link>
-            </>
+            <Link href="/pricing" className="dark:text-white">
+              Pricing
+            </Link>
           )}
           <DarkModeToggle />
 
           {/*user btn*/}
-          <UserButton />
+          <UserButton session={session} />
         </div>
       </nav>
 
--- components/UserButton/UserButton.tsx
+++ components/UserButton/UserButton.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import {
   DropdownMenu,
@@ -8,22 +10,41 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import UserAvatar from '@/components/UserAvatar/UserAvatar';
+import { Session } from 'next-auth';
+import { Button } from '@/components/ui/button';
+import { signIn, signOut } from 'next-auth/react';
 
-function UserButton() {
+function UserButton({ session }: { session: Session | null }) {
+  //subscription
+
+
   //Session
+  if (!session)
+    return (
+      <>
+        <Button
+          className="dark:text-white"
+          variant={'outline'}
+          onClick={() => signIn()}
+        >
+          Sign in
+        </Button>
+      </>
+    );
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <UserAvatar name="Ofir Somech" image="https://github.com/shadcn.png" />
+        <UserAvatar name={session?.user?.name!} image={session?.user?.image!} />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>Profile</DropdownMenuItem>
         <DropdownMenuItem>Billing</DropdownMenuItem>
         <DropdownMenuItem>Team</DropdownMenuItem>
         <DropdownMenuItem>Subscription</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
